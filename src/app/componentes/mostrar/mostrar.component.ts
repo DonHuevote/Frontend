6a57bbcbe6dd841cd1afa2c708fc1fbd61550472
Sopapa <extrahuevordinariopro@gmail.com>
2025-04-22 11:42:58 -0300

    mas lindo y con alertas, path guardado en variable

--- src/app/componentes/mostrar/mostrar.component.ts
+++ src/app/componentes/mostrar/mostrar.component.ts
@@ -4,35 +4,41 @@ import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import axios from 'axios';
 
-class Clase{
-  nombre:string;
-  duracion:number;
-  esOnline:boolean;
-  mostrar:boolean;
+let Url: string = 'http://localhost:3000/clases';
+
+
+
+class Clase {
+  nombre: string;
+  duracion: number;
+  esOnline: boolean;
+  mostrar: boolean;
   _id: string;
 
-  constructor(_id:string, nombre:string,duracion:number,esOnline:boolean)
-    {
-      this._id = _id
-      this.mostrar = false
-      this.nombre = nombre
-      this.duracion = duracion
-      this.esOnline = esOnline
-    }
+  constructor(_id: string, nombre: string, duracion: number, esOnline: boolean) {
+    this._id = _id;
+    this.mostrar = false;
+    this.nombre = nombre;
+    this.duracion = duracion;
+    this.esOnline = esOnline;
+  }
 }
 
 @Component({
   selector: 'app-mostrar',
   imports: [CommonModule, FormsModule],
   templateUrl: './mostrar.component.html',
-  styleUrl: './mostrar.component.css'
+  styleUrls: ['./mostrar.component.css']
 })
-
-
 export class MostrarComponent {
+
+  popUpMessage = '';
+  showSuccess = false;
+  showError = false;
   
+
   clases: Clase[] = [];
-  mostrar: boolean = false
+  mostrar: boolean = false;
 
   clase = {
     nombre: '',
@@ -40,84 +46,94 @@ export class MostrarComponent {
     esOnline: false
   };
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) { }
 
   ngOnInit() {
-    this.start();  
+    this.start();
   }
 
   start() {
-    axios.get('http://localhost:3000/clases')
-    .then((response: any) => {
-
-      this.clases = response.data;
-    })
-    .catch((error: any) => {
-
-      console.log(error);
-    })
-    .finally(() => {
-
-    });
+    axios.get(Url)
+      .then((response: any) => {
+        this.clases = response.data;
+      })
+      .catch((error: any) => {
+        console.log(error);
+        this.showPopUp(' ERROR FATAL: No se pudo conectar con express',true);
+        setTimeout(() => { this.showError = false; }, 3000);
+      });
   }
 
   submit() {
-    axios.post('http://localhost:3000/clases',this.clase)
-    .then((response: any) => {
-
-      this.clase = { nombre: '', duracion: 0, esOnline: false };
-      this.start()
-    })
-    .catch((error: any) => {
-
-      console.log(error);
-    })
-    .finally(() => {
-      
-    });
+    axios.post(Url,this.clase)
+      .then((response: any) => {
+        this.clase = { nombre: '', duracion: 0, esOnline: false };
+        this.start();
+        this.showPopUp('Clase añadida con éxito');
+      })
+      .catch((error: any) => {
+        let errorCode : number = error.response.status;
+        
+        console.log();
+        if(errorCode == 500){
+          this.showPopUp('Error al añadir la clase. Revise el formato de los valores', true);
+        }else{
+          this.showPopUp('Error del sistema, estamos trabajando en eso '+ error, true);
+        }
+        
+      });
   }
-
-  delete(_id:string){
-    axios.delete('http://localhost:3000/clases/'+_id)
-    .then((response: any) => {
-
-      this.clases = this.clases.filter(clase => clase.nombre !== _id);
-      console.log("clase eliminado con éxito");
-    })
-    .catch((error: any) => {
-
-      console.log(error);
-    })
-    .finally(() => {
-
-    });
-  }
-
-  
   
 
-  submit_update(_id:string, claseSeleccionada: Clase) {
+  delete(_id:string) {
+    axios.delete(Url + '/' + _id)
+      .then((response: any) => {
+        this.start(); // actualizamos bien la lista
+        this.showPopUp(' Clase eliminada correctamente');
+      })
+      .catch((error: any) => {
+        console.log(error);
+        this.showPopUp('Error al eliminar la clase', true);
+      });
+  }
+  
+
+  submit_update(_id: string, claseSeleccionada: Clase) {
     this.clase = { 
       nombre: claseSeleccionada.nombre, 
       duracion: claseSeleccionada.duracion, 
       esOnline: claseSeleccionada.esOnline 
     };
-    console.log(this.clase)
-    axios.put('http://localhost:3000/clases/'+_id,this.clase)
-    .then((response: any) => {
-
   
-      this.clase = { nombre: '', duracion: 0, esOnline: false };
-      this.start()
-    })
-    .catch((error: any) => {
-
-      console.log(error);
-    })
-    .finally(() => {
-      
-    });
+    axios.put(Url + '/' + _id, this.clase)
+      .then((response: any) => {
+        this.clase = { nombre: '', duracion: 0, esOnline: false };
+        this.start();
+        this.showPopUp(' Clase actualizada con éxito');
+      })
+      .catch((error: any) => {
+        let errorCode : number = error.response.status;
+        
+        console.log();
+        if(errorCode == 500){
+          this.showPopUp('Error al actualizar la clase. Revise el formato de los valores', true);
+        }else{
+          this.showPopUp('Error del sistema, estamos trabajando en eso '+ error, true);
+        }
+      });
   }
+  
+
+  showPopUp(message: string, isError: boolean = false) {
+    this.popUpMessage = message;
+    this.showSuccess = !isError;
+    this.showError = isError;
+  
+    setTimeout(() => {
+      this.showSuccess = false;
+      this.showError = false;
+      this.popUpMessage = '';
+    }, 3000);
+  }
+  
 }
-
-
